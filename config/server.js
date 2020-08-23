6a16a65bd6a186e59c1f48ff1e632c90df0e82c7
Andre <als.andresilveira@gmail.com>
2020-08-23 20:58:05 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a3d6dfaf62d90091aeed1b633801cbc1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bcaf35e7-cb3f-4530-94ea-c1f5e3dc6948'
+};
